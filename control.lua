f196662b7f771e8c3625ef3388b5c71630af2c4c
Fred Strauss <fred@strider.co.za>
2019-03-01 23:41:50 +0200

    More work on mod

--- control.lua
+++ control.lua
@@ -1,18 +1,10 @@
---[[
-
-* Research to enable this?
-* Button to toggle it on or off
-* Config the minimum of each item there should be
-* Config the number to craft
-
---]]
-
 script.on_event({defines.events.on_tick},
    function (e)
 		if e.tick % 60 == 0 then		-- only check every 1 second
 			local p = game.players[1]
+			local config = settings.get_player_settings(p)
 
-			local inventory = p.get_main_inventory()
+			if not config["quickbarcrafting-enabled"].value then return end
 
 			-- get the primary quick bar 
 			local qbp = p.get_active_quick_bar_page(1)*10
@@ -21,11 +13,17 @@ script.on_event({defines.events.on_tick},
 			for i = 1,10 do
 				local item = p.get_quick_bar_slot(qbp+i)
 				if item ~= nil then
-					-- if fewer than 5 of this item, craft 5 if possible
-					if p.get_item_count(item.name) < 5 and item.subgroup.name ~= "raw-resource" and p.crafting_queue_size == 0 then
-						if p.begin_crafting({count=5, recipe=item.name, silent=true}) > 0 then
-							p.print("Quick Bar Crafting " .. item.name)
-							return	-- stop at first item to craft
+					local minimum = config["quickbarcrafting-minimum"].value
+					local craftamount = config["quickbarcrafting-craft-amount"].value
+
+					if p.get_item_count(item.name) < minimum and item.subgroup.name ~= "raw-resource" and p.crafting_queue_size == 0 then
+						-- check if this item can be crafted
+						local recipe = game.recipe_prototypes[item.name]
+						if recipe ~= nil and recipe.allow_as_intermediate then
+							if p.begin_crafting({count=craftamount, recipe=item.name, silent=true}) > 0 then
+								p.print("Quick Bar Crafting " .. item.name)
+								return	-- stop at first item to craft
+							end
 						end
 					end
 				end
--- settings.lua
+++ settings.lua
@@ -3,6 +3,28 @@ data:extend({
 		name = "quickbarcrafting-enabled",
 		type = "bool-setting",
 		setting_type = "runtime-per-user",
-		default_value = true
+		default_value = true,
+		order = "a"
+	}
+})
+
+data:extend({
+	{
+		name = "quickbarcrafting-minimum",
+		type = "int-setting",
+		setting_type = "runtime-per-user",
+		default_value = 10,
+		order = "b"
+	}
+})
+
+
+data:extend({
+	{
+		name = "quickbarcrafting-craft-amount",
+		type = "int-setting",
+		setting_type = "runtime-per-user",
+		default_value = 5,
+		order = "c"
 	}
 })
