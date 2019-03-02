4052898a89b64f9eb8509674fa7102d4866f6e06
Fred Strauss <fred@strider.co.za>
2019-03-02 13:13:33 +0200

    Script to create zip files for distribution

---
+++ makezip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+VERSION=`grep version info.json | grep -v factorio | cut -d ':' -f 2 | sed 's/\"//g' | sed 's/^\s*//' | sed 's/,$//'`
+NAME="QuickBarCrafting_$VERSION"
+ZIPFILE="${NAME}.zip"
+
+rm -f /tmp/$ZIPFILE
+rm -rf /tmp/$NAME
+
+mkdir /tmp/$NAME
+cp -r *lua *json locale LICENSE /tmp/$NAME/
+
+pushd /tmp
+zip -r $ZIPFILE $NAME
+popd
+mv /tmp/$ZIPFILE .
